4ab645e10c82906236dae17007e9fd4d1b5b036e
TooManyBagels <chessyang88@gmail.com>
2026-08-09 16:30:37 -0500

    Framework for Simple Rune relic

---
+++ bagelsModCode/Ancients/Ancient_1.cs
@@ -0,0 +1,16 @@
+﻿public class MENITAS : CustomAncientModel
+{
+    protected override OptionPools MakeOptionPools => new OptionPools(
+        [
+            AncientOption<Nunchaku>(),
+            AncientOption<Lantern>(),
+            AncientOption<ArtOfWar>()
+            //more relic options
+        ]
+    );
+    
+    public override bool IsValidForAct(ActModel act)
+    {
+        return act.ActNumber() == 2;
+    }
+}
---
+++ bagelsModCode/Relics/SimpleRune.cs
@@ -0,0 +1,31 @@
+﻿using bagelsMod.bagelsModCode.Relics;
+using Godot;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace bagelsMod.bagelsModCode.Relics;
+
+public class SimpleRune() : bagelsModRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Ancient;
+
+    public override bool HasUponPickupEffect => true; 
+    
+    public override Task AfterObtained()
+    {
+        foreach (CardModel card in (IEnumerable<CardModel>) PileType.Deck.GetPile(this.Owner).Cards.ToList<CardModel>())
+        {
+            if (card.Rarity == CardRarity.Basic && (card.Tags.Contains<CardTag>(CardTag.Strike) || card.Tags.Contains<CardTag>(CardTag.Defend))
+            {
+                CardCmd.Upgrade(card);
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
