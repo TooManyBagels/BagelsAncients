fb4edf230042e286f840fc4919ea59d856ad752c
TooManyBagels <chessyang88@gmail.com>
2026-08-13 19:28:14 -0500

    Shattering Crystal Functionality

---
+++ bagelsModCode/Enchantments/Fragile.cs
@@ -0,0 +1,20 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Models;
+
+namespace bagelsMod.bagelsModCode.Enchantments;
+
+public class Fragile() : EnchantmentModel
+{
+    public override bool CanEnchantCardType(CardType cardType) => cardType is CardType.Skill or CardType.Attack;
+
+    public override bool CanEnchant(CardModel card)
+    {
+        return base.CanEnchant(card) && !card.GetKeywordsWithSources(KeywordSources.Local).Contains(CardKeyword.Exhaust);
+    }
+    
+    protected override void OnEnchant()
+    {
+        this.Card.AddKeyword(CardKeyword.Exhaust);
+        Card.EnergyCost.UpgradeBy(-1);
+    }
+}
---
+++ bagelsModCode/Relics/ShatteringCrystal.cs
@@ -0,0 +1,38 @@
+﻿using bagelsMod.bagelsModCode.Enchantments;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace bagelsMod.bagelsModCode.Relics;
+
+[Pool(typeof(SharedRelicPool))]
+public class ShatteringCrystal() : BagelsModRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+    
+    public override bool HasUponPickupEffect => true;
+    
+    public override async Task AfterObtained()
+    {
+        var prefs = new CardSelectorPrefs(CardSelectorPrefs.EnchantSelectionPrompt, 0, this.DynamicVars.Cards.IntValue)
+        {
+            Cancelable = false,
+            RequireManualConfirmation = true
+        };
+        var canonicalEnchantment = ModelDb.Enchantment<Fragile>();
+        foreach (var card in await CardSelectCmd.FromDeckForEnchantment(this.Owner, (EnchantmentModel) canonicalEnchantment, 3, prefs))
+        {
+            CardCmd.Enchant(canonicalEnchantment.ToMutable(), card, 1);
+            CardCmd.Preview(card);
+        }
+
+        canonicalEnchantment = null;
+    }
+}
