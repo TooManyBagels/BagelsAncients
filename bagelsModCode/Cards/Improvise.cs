2c9edcd2158dc0584dff2bc53751d6fa9ed2af5e
TooManyBagels <chessyang88@gmail.com>
2026-08-15 01:21:05 -0500

    Functionality for Improvise and Writhing Tongs

---
+++ bagelsModCode/Cards/Improvise.cs
@@ -0,0 +1,35 @@
+﻿using bagelsMod.bagelsModCode.Cards;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Extensions;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace bagelsMod.bagelsModCode.Cards;
+
+[Pool(typeof(EventCardPool))]
+public class Improvise() : bagelsModCard(0,
+    CardType.Skill, CardRarity.Ancient,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        var cardsToAdd = PileType.Draw.GetPile(this.Owner).Cards
+            .Where<CardModel>((Func<CardModel, bool>)(c => c.IsUpgradable)).ToList<CardModel>()
+            .StableShuffle<CardModel>(this.Owner.RunState.Rng.CombatCardSelection).Take<CardModel>(this.DynamicVars.Cards.IntValue);
+        foreach(var card in cardsToAdd)
+        {
+            await CardPileCmd.Add(card, PileType.Hand);
+            CardCmd.Upgrade(card);
+        }
+    }
+
+    protected override void OnUpgrade() => this.DynamicVars.Cards.UpgradeValueBy(1);
+}
---
+++ bagelsModCode/Relics/WrithingTongs.cs
@@ -0,0 +1,26 @@
+﻿using bagelsMod.bagelsModCode.Cards;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+
+namespace bagelsMod.bagelsModCode.Relics;
+
+[Pool(typeof(SharedRelicPool))]
+public class WrithingTongs() : BagelsModRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Ancient;
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => HoverTipFactory.FromCardWithCardHoverTips<Improvise>();
+
+    public override bool HasUponPickupEffect => true;
+
+    public override async Task AfterObtained()
+    {
+        var card = Owner.RunState.CreateCard<Improvise>(Owner);
+        CardCmd.PreviewCardPileAdd([await CardPileCmd.Add(card, PileType.Deck)], 2f);
+    }
+}
