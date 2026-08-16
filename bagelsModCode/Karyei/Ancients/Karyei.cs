2114fde0f322f59f9e70fc5d4ebc66828b60e758
TooManyBagels <chessyang88@gmail.com>
2026-08-16 14:03:12 -0500

    Double Pendulum functionality
    Signed-off-by: TooManyBagels <chessyang88@gmail.com>

---
+++ bagelsModCode/Karyei/Ancients/Karyei.cs
@@ -0,0 +1,30 @@
+﻿using bagelsMod.bagelsModCode.Karyei.Relics;
+using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+
+namespace bagelsMod.bagelsModCode.Karyei.Ancients;
+
+public class Karyei : CustomAncientModel
+{
+    public override bool IsValidForAct(ActModel act)
+    {
+        return act.ActNumber() == 2;
+    }
+
+    protected override OptionPools MakeOptionPools => new OptionPools(
+        [
+            AncientOption<BlightedMushroom>(),
+            AncientOption<Vajra>(),
+            AncientOption<OddlySmoothStone>()
+        ]
+    );
+    
+    public override string CustomScenePath => "res://bagelsMod/scenes/bagelsmod-menitas.tscn";
+    public override string CustomMapIconPath => "res://bagelsMod/images/ancients/menitas.png";
+    public override string CustomMapIconOutlinePath => "res://bagelsMod/images/ancients/menitas_outline.png";
+    public override string CustomRunHistoryIconPath => "res://bagelsMod/images/ancients/menitas-pfp.png";
+    public override string CustomRunHistoryIconOutlinePath => "res://bagelsMod/images/ancients/menitas-pfp_outline.png";
+}
---
+++ bagelsModCode/Karyei/Relics/BlightedMushroom.cs
@@ -0,0 +1,27 @@
+﻿using bagelsMod.bagelsModCode.Relics;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace bagelsMod.bagelsModCode.Karyei.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class BlightedMushroom : BagelsModRelic
+{
+    public override RelicRarity Rarity =>
+        RelicRarity.Ancient;
+
+    public override bool HasUponPickupEffect => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new MaxHpVar(66), new DamageVar(66, ValueProp.Unblockable)];
+    
+    public override async Task AfterObtained()
+    {
+        await CreatureCmd.GainMaxHp(Owner.Creature, DynamicVars.MaxHp.BaseValue);
+        await CreatureCmd.Damage(new ThrowingPlayerChoiceContext(), Owner.Creature, DynamicVars.Damage, null, null, null);
+    }
+}
