127cb9793142d9fc83fcc055b966777e5fb3b859
TooManyBagels <chessyang88@gmail.com>
2026-09-02 17:41:14 -0500

    PandorasBrew.cs now grants 2 energy
    Signed-off-by: TooManyBagels <chessyang88@gmail.com>

--- bagelsModCode/Karyei/Potions/PandorasBrew.cs
+++ bagelsModCode/Karyei/Potions/PandorasBrew.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Potions;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.PotionPools;
 
 namespace bagelsMod.bagelsModCode.Karyei.Potions;
@@ -13,13 +14,19 @@ namespace bagelsMod.bagelsModCode.Karyei.Potions;
 public class PandorasBrew : CustomPotionModel
 {
     public override PotionRarity Rarity => PotionRarity.Token;
-    
+
     public override PotionUsage Usage => PotionUsage.CombatOnly;
     
     public override TargetType TargetType => TargetType.AnyPlayer;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new EnergyVar(2)
+    ];
     
     protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
     {
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, Owner);
         var cardsInHand = PileType.Hand.GetPile(Owner).Cards.ToList();
         foreach (var c in cardsInHand) 
             await CardCmd.TransformToRandom(c, Owner.RunState.Rng.CombatCardSelection);
