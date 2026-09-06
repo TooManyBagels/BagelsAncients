c7784ca9e2438bf95d6c9a2d6413e93e01227f45
TooManyBagels <chessyang88@gmail.com>
2026-09-06 16:06:41 -0500

    Updated Obsession
    Signed-off-by: TooManyBagels <chessyang88@gmail.com>

--- bagelsModCode/Karyei/Cards/Obsession.cs
+++ bagelsModCode/Karyei/Cards/Obsession.cs
@@ -3,32 +3,29 @@ using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.CardPools;
 
 namespace bagelsMod.bagelsModCode.Karyei.Cards;
 
 [Pool(typeof(CurseCardPool))]
-public class Obsession() : BagelsModCard(-1,
+public class Obsession() : BagelsModCard(1,
     CardType.Curse, CardRarity.Curse,
     TargetType.None)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
-    
     public override int MaxUpgradeLevel => 0;
 
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Eternal, CardKeyword.Unplayable];
-    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Eternal];
+
     public override bool HasTurnEndInHandEffect => true;
-    
-    public override async Task AfterCardDrawn(
-        PlayerChoiceContext choiceContext,
-        CardModel card,
-        bool fromHandDraw)
+
+    protected override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
     {
-        if (card != this)
-            return;
-        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CreateClone(), PileType.Draw, Owner));
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CreateClone(), PileType.Discard, Owner));
+    }
+    
+    public override async Task AfterCardDrawn(PlayerChoiceContext choiceContext, CardModel card, bool fromHandDraw)
+    {
+        if(card == this) CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CreateClone(), PileType.Discard, Owner));
     }
 }
